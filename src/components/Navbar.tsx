3558fd64eb7e828cc194bfa84f162657eeb306aa
Bryan Gordon <bryanbge92@gmail.com>
2024-12-24 18:59:48 -0500

    Create navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+export function Navbar () {
+  return (
+    <nav>
+      <ul className='w-full bg-cyan-600 flex text-red-600'>
+        <li className='text-red-500'>
+          Home
+        </li>
+        <li>
+          Servicios
+        </li>
+        <li>
+          Contactos
+        </li>
+      </ul>
+    </nav>
+  )
+}
