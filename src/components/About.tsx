d9f251ac6fd998b5ae15b2538dc51958d2e80ec1
Bryan Gordon <bryanbge92@gmail.com>
2024-12-26 18:11:56 -0500

    Update component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,15 @@
 export function About () {
   return (
     <section>
-      Seccion sobre nosotros.
+      <h1 className='text-center text-lg mt-16 text-bodyText '>Sobre Nosotros</h1>
+      <article className='mt-10 '>
+        <picture className='w-full mb-10'>
+          <img className='w-96 min-w-48 h-96 my-0 mx-auto' src='gimnasio.jpeg' alt='imagen' />
+        </picture>
+        <div className='w-3/4 my-0 mx-auto'>
+          <p className='text-center text-base text-bodyText'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad porro, magni voluptatum ullam at consequatur in amet velit eius reiciendis eveniet quia odit suscipit sunt perspiciatis libero incidunt. Natus voluptatem nostrum nesciunt ipsam fugit illo quod magnam odio. Hic, nesciunt!</p>
+        </div>
+      </article>
     </section>
   )
 }
