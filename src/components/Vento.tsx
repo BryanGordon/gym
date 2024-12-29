280a4f8ba680d1f01f5f72bf02b909b0e750dd38
Bryan Gordon <bryanbge92@gmail.com>
2024-12-28 19:08:31 -0500

    Update bento component

--- src/components/Vento.tsx
+++ src/components/Vento.tsx
@@ -1,5 +1,12 @@
 export function Vento () {
   return (
-    <>Vento</>
+    <section className='mb-24'>
+      <article className='grid grid-cols-2 text-bodyText w-3/4 my-0 mx-auto'>
+          <img src='ejercicio.webp' alt='imagen' />
+          <img src='entrenamiento.jpg' alt='imagen' />
+          <img src='gym-material.webp' alt='imagen' />
+          <img src='trainer.jpg' alt='imagen' />
+      </article>
+    </section>
   )
 }
