5bf9cc46d21ed7a129c46202fc2d995e66061654
Bryan Gordon <bryanbge92@gmail.com>
2024-12-28 19:07:33 -0500

    Update app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { About } from './components/About'
+import { Footer } from './components/Footer'
 import { Navbar } from './components/Navbar'
+import { Vento } from './components/Vento'
 
 function App () {
   return (
-    <>
+    <main>
       <Navbar />
       <About />
-    </>
+      <Vento />
+      <Footer />
+    </main>
   )
 }
 
