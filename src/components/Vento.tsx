4f4be9012aff2bfd616375c20739e3903fceef1d
Bryan Gordon <bryanbge92@gmail.com>
2024-12-30 19:27:40 -0500

    Add images styles

--- src/components/Vento.tsx
+++ src/components/Vento.tsx
@@ -1,11 +1,11 @@
 export function Vento () {
   return (
     <section className='mb-24'>
-      <article className='grid grid-cols-2 text-bodyText w-3/4 my-0 mx-auto'>
-          <img src='ejercicio.webp' alt='imagen' />
-          <img src='entrenamiento.jpg' alt='imagen' />
-          <img src='gym-material.webp' alt='imagen' />
-          <img src='trainer.jpg' alt='imagen' />
+      <article className='text-bodyText w-3/4 my-0 mx-auto grid grid-cols-2 gap-5'>
+          <img className='rounded-md w-full h-[440px]' src='ejercicio.webp' alt='imagen' />
+          <img className='rounded-md w-full h-[440px]' src='entrenamiento.jpg' alt='imagen' />
+          <img className='rounded-md w-full h-[440px]' src='gym-material.webp' alt='imagen' />
+          <img className='rounded-md w-full h-[440px]' src='trainer.jpg' alt='imagen' />
       </article>
     </section>
   )
