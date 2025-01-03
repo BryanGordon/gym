e150a10b3d7ab0c18034ba2d414e7f9c7ef6d465
Bryan Gordon <bryanbge92@gmail.com>
2025-01-02 19:24:35 -0500

    Add user icon

---
+++ src/icons/Person.tsx
@@ -0,0 +1,18 @@
+export function PersonIcon () {
+  return (
+    <svg  
+      width="27"  
+      height="27"  
+      viewBox="0 0 23 23"  
+      fill="none"  
+      stroke="currentColor"  
+      stroke-width="1.5"  
+      stroke-linecap="round"  
+      stroke-linejoin="round"  
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+      <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+    </svg>
+  )
+}
