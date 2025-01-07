3fd94322f637578ff6a7a2417415adc751c0ae39
Bryan Gordon <bryanbge92@gmail.com>
2025-01-07 18:54:05 -0500

    Update footer structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,26 @@
+import { FacebookIcon } from '../icons/Facebook'
+import { InstagramIcon } from '../icons/Instagram'
+import { WhatsappIcon } from '../icons/Whatsapp'
+
 export function Footer () {
   return (
-    <footer className='w-full p-10 bg-red-600 mt-auto'>
-      <span className='text-center'>Footer</span>
+    <footer className='w-full p-4 bg-cyan-600 mt-auto'>
+      <div className='flex justify-between px-2'>
+        <span className='content-center text-lg'>Gym</span>
+        <span className='content-center text-lg'>Volver al inicio</span>
+
+        <div className='flex'>
+          <a className='pl-2 pr-2' href='#'>
+            <i><FacebookIcon /></i>
+          </a>
+          <a className='pl-2 pr-2 mx-2' href='#'>
+            <i><InstagramIcon /></i>
+          </a>
+          <a className='pl-2 pr-2' href='#'>
+            <i><WhatsappIcon /></i>
+          </a>
+        </div>
+      </div>
     </footer>
   )
 }
