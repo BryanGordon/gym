5dc061d8da8d0fc227179fa1a3ffd864c1d9f6ef
Bryan Gordon <bryanbge92@gmail.com>
2025-01-06 19:57:49 -0500

    Update styles

--- src/components/Habilities.tsx
+++ src/components/Habilities.tsx
@@ -12,30 +12,30 @@ export function Habilities () {
   return (
     <section className='mb-24'>
       <h1 className='text-bodyText text-center mb-10'>Mejora con nosotros</h1>
-      <article className='w-3/4 h-[600px] my-0 mx-auto grid grid-cols-2 grid-rows-3 gap-5'>
+      <article className='w-3/4 h-[600px] my-0 mx-auto grid grid-cols-2 grid-rows-3 gap-2'>
 
-        <div className='col-span-1 row-span-2 bg-orange-500  '>
+        <div className='col-span-1 row-span-2 bg-orange-500 rounded-xl'>
           <div className='pl-5 pt-4 flex'>
             <BarbellIcon />
-            <span className='ml-5'>Ejercicios pensados para tus necesidades</span>
+            <span className='ml-5 text-bentoTitle'>Ejercicios pensados para tus necesidades</span>
           </div>
 
           <div>
             <picture className='flex flex-col h-3/4 justify-start place-items-center pt-10 mb-9'>
               <img className='w-96 h-52' src={Exercie} alt='imagen' />
             </picture>
-            <p className='px-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?</p>
+            <p className='px-6 text-bentoTitle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate fugit doloribus dolor voluptatum porro at?</p>
           </div>
         </div>
 
-        <div className='col-span-1 row-span-1 bg-green-400 overflow-hidden'>
+        <div className='col-span-1 row-span-1 bg-green-400 rounded-xl overflow-hidden'>
           <div className='pl-5 pt-4 flex'>
             <SaladIcon />
-            <span className='ml-5'>Dietas aliementicias a medida</span>
+            <span className='ml-5 text-bentoTitle font-semibold'>Dietas aliementicias a medida</span>
           </div>
 
           <div className='flex'>
-            <p className='w-3/4 pt-10 px-6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae adipisci, nesciunt a odit cumque exercitationem.</p>
+            <p className='w-3/4 pt-10 px-6 font-medium text-gray-600'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae adipisci, nesciunt a odit cumque exercitationem.</p>
             <picture>
               <img src={Salad} alt='imagen' />
             </picture>
@@ -43,24 +43,24 @@ export function Habilities () {
 
         </div>
 
-        <div className='col-span-1 row-span-1 bg-indigo-400'>
+        <div className='col-span-1 row-span-1 bg-indigo-400 rounded-xl'>
           <div className='pl-5 pt-4 flex'>
             <PersonIcon />
-            <span className='ml-5'>Entrenadores personales a tu disposicion</span>
+            <span className='ml-5 text-bentoTitle'>Entrenadores personales a tu disposicion</span>
           </div>
 
-          <div className='flex justify-evenly '>
-            <p className='w-3/5 pt-10 px-6'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, non vel amet illum maiores autem!</p>
+          <div className='flex justify-eveny'>
+            <p className='w-3/5 pt-10 px-6 font-normal'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, non vel amet illum maiores autem!</p>
             <picture className='w-1/4'>
               <img className='pb-5 max-h-[150px]' src={Muscle} alt='image' />
             </picture>
           </div>
         </div>
 
-        <div className='col-span-2 row-span-1 bg-rose-400 overflow-hidden'>
+        <div className='col-span-2 row-span-1 bg-rose-400 rounded-xl overflow-hidden'>
           <div className='pl-5 pt-4 flex'>
             <ClockIcon />
-            <span className='ml-5'>Horario flexibles. Ajustalo como quieras!!!</span>
+            <span className='ml-5 text-bentoTitle'>Horario flexibles. Ajustalo como quieras!!!</span>
           </div>
 
           <div className='flex'>
