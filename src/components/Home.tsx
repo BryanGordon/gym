95b35a5b619f6fc95c2550cccaf0faa8d8b646d5
Bryan Gordon <bryanbge92@gmail.com>
2025-01-08 18:22:13 -0500

    Refactor main app

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,13 @@
+import { About } from './About'
+import { Collage } from './Collage'
+import { Habilities } from './Habilities'
+
 export function Home () {
   return (
     <>
-
+      <About />
+      <Collage />
+      <Habilities />
     </>
   )
 }
