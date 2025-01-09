6423957237b1c6e469e26878f9de761fcdf77ba4
Bryan Gordon <bryanbge92@gmail.com>
2025-01-09 18:44:38 -0500

    Update app structure
    - Add new page route
    - Add 404 error page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 import { Footer } from './components/Footer'
 import { Navbar } from './components/Navbar'
 import { Home } from './components/Home'
+import { Page404 } from './components/404'
+import { Servicios } from './components/Servicios'
 
 function App () {
   return (
@@ -10,7 +12,9 @@ function App () {
       <main>
         <Navbar />
         <Routes>
+          <Route path='*' element={<Page404 />} />
           <Route path='/' element={<Home />} />
+          <Route path='/servicios' element={<Servicios />} />
         </Routes>
         <Footer />
       </main>
