08e813c297b6f02f549e7dbcb0604f5322c14f53
Bryan Gordon <bryanbge92@gmail.com>
2025-01-10 18:56:00 -0500

    Create locations components

---
+++ src/components/Sedes.tsx
@@ -0,0 +1,51 @@
+import GymView from '/outside-left-view.webp'
+import GymView2 from '/outside-right-view.jpg'
+
+export function Sedes () {
+  return (
+    <section className='mb-24'>
+      <h1 className='text-bodyText text-h1 mt-16 text-center'>Nuestras sedes</h1>
+      <article className='mt-10'>
+        <div className='w-3/4 flex flex-col my-0 mx-auto'>
+          <h3 className='text-bodyText text-h3 mb-8'>Ibarra</h3>
+          <picture>
+            <img className='w-[1250px] min-w-48 h-[580px] rounded-lg' src={GymView} alt='image' />
+          </picture>
+          <p className='text-bodyText text-center py-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, ipsa!</p>
+        </div>
+
+        <div className='w-3/4 flex flex-col my-0 mx-auto'>
+          <h3 className='text-bodyText text-h3 mb-8'>Quito</h3>
+          <picture>
+            <img className='w-[1250px] min-w-48 h-[580px] rounded-lg' src={GymView2} alt='image' />
+          </picture>
+          <p className='text-bodyText text-center py-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, inventore.</p>
+        </div>
+
+        <div className='w-3/4 my-0 mx-auto'>
+          <h3 className='text-bodyText text-h3 mt-10 text-center'>Contactos</h3>
+          <div className='flex justify-around mt-14'>
+            <ul className='text-bodyText *:py-3'>
+              <li>Otro</li>
+              <li>Otro</li>
+              <li>Otro</li>
+            </ul>
+
+            <ul className='text-bodyText *:py-3'>
+              <li>Otro</li>
+              <li>Otro</li>
+              <li>Otro</li>
+            </ul>
+
+            <ul className='text-bodyText *:py-3'>
+              <li>Otro</li>
+              <li>Otro</li>
+              <li>Otro</li>
+            </ul>
+          </div>
+        </div>
+
+      </article>
+    </section>
+  )
+}
