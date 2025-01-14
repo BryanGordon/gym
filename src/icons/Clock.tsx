777582bdbe627023ce956b8cbeff99199aca98b7
Bryan Gordon <bryanbge92@gmail.com>
2025-01-13 19:23:49 -0500

    Format code

--- src/icons/Clock.tsx
+++ src/icons/Clock.tsx
@@ -1,18 +1,18 @@
 export function ClockIcon () {
   return (
-    <svg  
-      width="27"  
-      height="27"  
-      viewBox="0 0 23 23"  
-      fill="none"  
-      stroke="currentColor"  
-      stroke-width="1.2"  
-      stroke-linecap="round"  
-      stroke-linejoin="round"  
+    <svg
+      width='27'
+      height='27'
+      viewBox='0 0 23 23'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='1.2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-      <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
-      <path d="M12 7v5l3 3" />
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0' />
+      <path d='M12 7v5l3 3' />
     </svg>
   )
 }
--- src/icons/Instagram.tsx
+++ src/icons/Instagram.tsx
@@ -1,20 +1,19 @@
 export function InstagramIcon () {
   return (
-    <svg  
-    width="26"  
-    height="26"  
-    viewBox="0 0 24 24"  
-    fill="none"  
-    stroke="currentColor"  
-    stroke-width="1.9"  
-    stroke-linecap="round"  
-    stroke-linejoin="round"  
-    class="icon icon-tabler icons-tabler-outline icon-tabler-brand-instagram"
+    <svg
+      width='26'
+      height='26'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='1.9'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-      <path d="M4 8a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
-      <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
-      <path d="M16.5 7.5v.01" />
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M4 8a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z' />
+      <path d='M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0' />
+      <path d='M16.5 7.5v.01' />
     </svg>
   )
 }
