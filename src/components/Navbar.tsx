265f469e8aed4f85f2c0a3a9988dad76a349faab
Bryan Gordon <bryanbge92@gmail.com>
2025-01-13 19:23:28 -0500

    Change color background

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 import { NavLink } from 'react-router-dom'
-
+// #B4B4B4
 export function Navbar () {
   return (
     <nav>
-      <ul className='w-full py-8 bg-cyan-600 flex justify-around'>
+      <ul className='w-full py-8 bg-[#8F90B5] flex justify-around'>
         <li>
           <NavLink to='/'>Home</NavLink>
         </li>
