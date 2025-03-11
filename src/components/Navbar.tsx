038426ca22b49db59dd5350f99c71b0c2425cb15
Bryan Gordon <bryanbge92@gmail.com>
2025-03-10 20:51:10 -0500

    Add classes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,13 @@ export function Navbar () {
   return (
     <nav>
       <ul className='w-full py-8 bg-[#8F90B5] flex justify-around'>
-        <li>
+        <li className='text-navText'>
           <NavLink className='navbar-element' to='/'>Home</NavLink>
         </li>
-        <li>
+        <li className='text-navText'>
           <NavLink className='navbar-element' to='/servicios'>Servicios</NavLink>
         </li>
-        <li>
+        <li className='text-navText'>
           <NavLink className='navbar-element' to='/contactos'>Contactos</NavLink>
         </li>
       </ul>
