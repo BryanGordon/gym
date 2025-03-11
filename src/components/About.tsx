1e7fd64dc8f32a0f79c805a9a3239a94406ed5cd
Bryan Gordon <bryanbge92@gmail.com>
2025-03-10 20:50:39 -0500

    Update components styles

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,8 +8,8 @@ export function About () {
       </header>
 
       <article className='mt-10 '>
-        <picture className='w-full mb-10'>
-          <img className='w-[1250px] min-w-48 h-[580px] my-0 mx-auto rounded-md' src={Gym} alt='Gym entrance' />
+        <picture>
+          <img className='w-[1250px] min-w-48 h-[580px] my-0 mx-auto rounded-md shadow-[#131313f8] shadow-xl' src={Gym} alt='Gym entrance' />
         </picture>
         <div className='w-3/4 my-0 mx-auto pt-14'>
           <p className='text-center text-base text-bodyText'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad porro, magni voluptatum ullam at consequatur in amet velit eius reiciendis eveniet quia odit suscipit sunt perspiciatis libero incidunt. Natus voluptatem nostrum nesciunt ipsam fugit illo quod magnam odio. Hic, nesciunt!</p>
