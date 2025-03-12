9b9320a834e04d4b7daa16027df6296e94ac5cd1
Bryan Gordon <bryanbge92@gmail.com>
2025-03-11 20:53:01 -0500

    Update styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,19 +4,23 @@ import { WhatsappIcon } from '../icons/Whatsapp'
 
 export function Footer () {
   return (
-    <footer className='w-full p-4 bg-[#8F90B5] mt-auto'>
+    <footer className='w-full p-6 mt-auto bg-gradient-to-b from-[#000034] via-[#3B0050] to-[#620089]'>
       <div className='flex justify-between px-2'>
-        <span className='content-center text-lg'>Gym</span>
-        <span className='content-center text-lg'>Volver al inicio</span>
+        <span className='content-center text-navText'>Gym</span>
+        <span className='content-center text-navText'>
+          <a href='#top'>
+            Volver al inicio
+          </a>
+        </span>
 
         <div className='flex'>
-          <a className='pl-2 pr-2' href='#'>
+          <a className='pl-2 pr-2 hover:scale-125 transition ease-in-out duration-200' href='#'>
             <i><FacebookIcon /></i>
           </a>
-          <a className='pl-2 pr-2 mx-2' href='#'>
+          <a className='pl-2 pr-2 mx-2 hover:scale-125 transition ease-in-out duration-200' href='#'>
             <i><InstagramIcon /></i>
           </a>
-          <a className='pl-2 pr-2' href='#'>
+          <a className='pl-2 pr-2 hover:scale-125 transition ease-in-out duration-200' href='#'>
             <i><WhatsappIcon /></i>
           </a>
         </div>
