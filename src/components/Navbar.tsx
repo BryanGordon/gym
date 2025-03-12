75d53012943b06d70221c1fccdf77d84d72a2cc8
Bryan Gordon <bryanbge92@gmail.com>
2025-03-11 20:53:11 -0500

    Update styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import { NavLink } from 'react-router-dom'
 // #B4B4B4
+// #620089
+// #000034
 export function Navbar () {
   return (
-    <nav>
-      <ul className='w-full py-8 bg-[#8F90B5] flex justify-around'>
+    <nav id='top'>
+      <ul className='w-full py-8 bg-gradient-to-t from-[#000034] via-[#3B0050] to-[#620089] flex justify-around'>
         <li className='text-navText'>
           <NavLink className='navbar-element' to='/'>Home</NavLink>
         </li>
