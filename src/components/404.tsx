3e4d563cf4ede01c2f17f83877ce41102ea439a6
Bryan Gordon <bryanbge92@gmail.com>
2025-03-12 17:39:58 -0500

    Update 404 page

--- src/components/404.tsx
+++ src/components/404.tsx
@@ -1,7 +1,12 @@
+import NotFoundImage from '../assets/404-image.jpg'
+
 export function Page404 () {
   return (
-    <>
-      <span className='text-h1 text-yellow-400'>La pagina no existe</span>
-    </>
+    <section className='w-3/4 my-0 mx-auto mt-10 text-center'>
+      <span className='text-h1 text-white'>La pagina no existe</span>
+      <div className='my-20 '>
+        <img className='rounded-lg my-0 mx-auto' src={NotFoundImage} alt='image' />
+      </div>
+    </section>
   )
 }
