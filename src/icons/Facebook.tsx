f4dd4a0ab45d9c2ad8c6309e1cf80ab67e67f93d
Bryan Gordon <bryanbge92@gmail.com>
2025-03-11 20:53:41 -0500

    Change icon color
    - Add styles

--- src/icons/Facebook.tsx
+++ src/icons/Facebook.tsx
@@ -5,10 +5,11 @@ export function FacebookIcon () {
       height='25'
       viewBox='0 0 24 24'
       fill='none'
-      stroke='currentColor'
+      stroke='#4267B2'
       strokeWidth='1.7'
       strokeLinecap='round'
       strokeLinejoin='round'
+      className='hover:stroke-[#dabef4d6] transition ease-linear duration-200'
     >
       <path stroke='none' d='M0 0h24v24H0z' fill='none' />
       <path d='M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3' />
