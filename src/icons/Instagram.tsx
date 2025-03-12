a5ba041d308dd5ef86e60aeb0c41b2942ed1d763
Bryan Gordon <bryanbge92@gmail.com>
2025-03-11 20:54:26 -0500

    Update icon color
    - Add styles

--- src/icons/Instagram.tsx
+++ src/icons/Instagram.tsx
@@ -5,10 +5,11 @@ export function InstagramIcon () {
       height='26'
       viewBox='0 0 24 24'
       fill='none'
-      stroke='currentColor'
+      stroke='#EA485D'
       strokeWidth='1.9'
       strokeLinecap='round'
       strokeLinejoin='round'
+      className='hover:stroke-[#dabef4d6] transition ease-linear duration-200'
     >
       <path stroke='none' d='M0 0h24v24H0z' fill='none' />
       <path d='M4 8a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z' />
