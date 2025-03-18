032202db1227bae3c27a647026ad90990ddf3233
Bryan Gordon <bryanbge92@gmail.com>
2025-03-17 20:49:07 -0500

    Add icon styles

--- src/icons/Clock.tsx
+++ src/icons/Clock.tsx
@@ -9,6 +9,7 @@ export function ClockIcon () {
       strokeWidth='1.2'
       strokeLinecap='round'
       strokeLinejoin='round'
+      className=' max-[720px]:stroke-2 max-[550px]:w-[38px] max-[550px]:h-[38px] max-[450px]:mb-2'
     >
       <path stroke='none' d='M0 0h24v24H0z' fill='none' />
       <path d='M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0' />
