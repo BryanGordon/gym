3dd110bb3be166aea50e1d1f5a1b68b187c6c703
Bryan Gordon <bryanbge92@gmail.com>
2025-03-17 20:49:22 -0500

    Add responsive icon styles

--- src/icons/Person.tsx
+++ src/icons/Person.tsx
@@ -9,6 +9,7 @@ export function PersonIcon () {
       strokeWidth='1.5'
       strokeLinecap='round'
       strokeLinejoin='round'
+      className='max-[720px]:stroke-2 max-[550px]:w-[38px] max-[550px]:h-[38px] max-[450px]:mb-2 max-[450px]:w-[35px] max-[450px]:h-[35px]'
     >
       <path stroke='none' d='M0 0h24v24H0z' fill='none' />
       <path d='M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0' />
