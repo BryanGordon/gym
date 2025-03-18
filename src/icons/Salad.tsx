6525c64b7fb94fae914bf6b9cae41746fd69a572
Bryan Gordon <bryanbge92@gmail.com>
2025-03-17 20:49:43 -0500

    Add responsive icon styles

--- src/icons/Salad.tsx
+++ src/icons/Salad.tsx
@@ -9,6 +9,7 @@ export function SaladIcon () {
       strokeWidth='1.2'
       strokeLinecap='round'
       strokeLinejoin='round'
+      className='max-[720px]:stroke-2 max-[550px]:stroke-[1.8] max-[550px]:w-[30px] max-[550px]:h-[30px] max-[450px]:mb-2 max-[450px]:w-[35px] max-[450px]:h-[35px]'
     >
       <path stroke='none' d='M0 0h24v24H0z' fill='none' />
       <path d='M4 11h16a1 1 0 0 1 1 1v.5c0 1.5 -2.517 5.573 -4 6.5v1a1 1 0 0 1 -1 1h-8a1 1 0 0 1 -1 -1v-1c-1.687 -1.054 -4 -5 -4 -6.5v-.5a1 1 0 0 1 1 -1z' />
